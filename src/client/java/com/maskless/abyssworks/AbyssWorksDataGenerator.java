c21872f3041281809b23860af2a7ab1eb6816804
Illina Burlace <chloeburlace@gmail.com>
2026-02-15 18:12:13 +0000

    Initial Commit

---
+++ src/client/java/com/maskless/abyssworks/AbyssWorksDataGenerator.java
@@ -0,0 +1,11 @@
+package com.maskless.abyssworks;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AbyssWorksDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
